6a7e2f8ce63f9ff495427f2e8823f7ce86a7d342
yuanying <yuanying@fraction.jp>
2013-07-14 13:02:06 +0900

    Bump nise_bosh cookbook.

--- config/chef.rb
+++ config/chef.rb
@@ -3,7 +3,6 @@ set :application,     "AphraelServer"
 run_list :router, [
   'role[bootstrap]',
   'recipe[nats]',
-  'recipe[nise_bosh]',
   'recipe[nise_bosh::deploy]',
   'recipe[docker]',
   'recipe[virtualbox]'
